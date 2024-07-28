28ecfc0265fa0e1c532d396a5dff669cfe94efee
unknown00p <rajpoint8976@gmail.com>
2024-07-28 12:45:13 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,9 @@
 import Header from "./components/user-Components/Header";
-import Home from "./components/user-Components/Home";
 import { Button } from "./components/ui/button";
-import HomeScreen from "./pages/HomeScreen";
 export default function App() {
 	return (
 		<div>
 			<Header/>
-			<Home/>
 		</div>
 	);
 }
